f3943d140a11f8e05afa9eb8170b1ed021357741
Peter Morgan <Peter.Morgan@datacom.com>
2024-11-19 19:22:50 +1300

    Added Interface Segregation Principle demo and unit tests.

---
+++ Tests/InterfaceSegregationUnitTests.cs
@@ -0,0 +1,80 @@
+﻿using solid_principles_in_c_sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace solid_principles_in_c_sharp.Tests
+{
+    public class InterfaceSegregationUnitTests
+    {
+        [Fact]
+        public void Programmer_WorkOnTask_DoesNotThrowException()
+        {
+            // Arrange
+            var programmer = new Programmer();
+
+            // Act & Assert
+            var exception = Record.Exception(() => programmer.WorkOnTask());
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void Manager_AssignTask_DoesNotThrowException()
+        {
+            // Arrange
+            var manager = new Manager();
+
+            // Act & Assert
+            var exception = Record.Exception(() => manager.AssignTask());
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void Manager_CreateSubTask_DoesNotThrowException()
+        {
+            // Arrange
+            var manager = new Manager();
+
+            // Act & Assert
+            var exception = Record.Exception(() => manager.CreateSubTask());
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void TeamLead_AssignTask_DoesNotThrowException()
+        {
+            // Arrange
+            var teamLead = new TeamLead();
+
+            // Act & Assert
+            var exception = Record.Exception(() => teamLead.AssignTask());
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void TeamLead_CreateSubTask_DoesNotThrowException()
+        {
+            // Arrange
+            var teamLead = new TeamLead();
+
+            // Act & Assert
+            var exception = Record.Exception(() => teamLead.CreateSubTask());
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void TeamLead_WorkOnTask_DoesNotThrowException()
+        {
+            // Arrange
+            var teamLead = new TeamLead();
+
+            // Act & Assert
+            var exception = Record.Exception(() => teamLead.WorkOnTask());
+            Assert.Null(exception);
+        }
+    }
+}
+
---
+++ solid-principles-in-c-sharp/InterfaceSegregation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid_principles_in_c_sharp
+{
+    public interface IProgrammer
+    {
+        void WorkOnTask();
+    }
+
+    public interface ILead
+    {
+        void AssignTask();
+        void CreateSubTask();
+    }
+
+    public class Programmer : IProgrammer
+    {
+        public void WorkOnTask()
+        {
+            //code to implement to work on the Task.
+        }
+    }
+    public class Manager : ILead
+    {
+        public void AssignTask()
+        {
+            //Code to assign a Task
+        }
+        public void CreateSubTask()
+        {
+            //Code to create a sub taks from a task.
+        }
+    }
+
+    public class TeamLead : IProgrammer, ILead
+    {
+        public void AssignTask()
+        {
+            //Code to assign a Task
+        }
+        public void CreateSubTask()
+        {
+            //Code to create a sub task from a task.
+        }
+        public void WorkOnTask()
+        {
+            //code to implement to work on the Task.
+        }
+    }
+}
